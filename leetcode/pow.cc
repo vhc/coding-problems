a3fad4b86308f6951723eb9ddba498dd7c2c99ba
vchiem <vu.h.chiem@gmail.com>
2016-04-17 16:30:21 -0700

    Solved LeetCode's Pow(x, n)

--- leetcode/pow.cc
+++ leetcode/pow.cc
@@ -1,29 +1,29 @@
-// Still failing: 34.00515 ^ (-3)
+#include <iostream>
+#include <cstdio>
 
 class Solution {
 public:
 	double myPow(double x, int input_n) {
-		if (input_n < 0) {
-			return 0;
-		} else if (input_n == 0) {
+		if (input_n == 0) {
 			return 1;
 		} else if (input_n == 1) {
 			return x;
 		}
-		
+
 		bool is_num_negative = (x < 0.0);
 		if (is_num_negative) {
 			x = -x;
 		}
-		
-		bool is_power_negative = (input_n < 0);
+
+		// Convert to 64-bit early to avoid overflow on the negation
+		int64_t n = (int64_t)input_n;
+		bool is_power_negative = (n < 0);
 		if (is_power_negative) {
-			input_n = -input_n;
+			n = -n;
 		}
-		
+
 		double result = x;
 		uint64_t p = 2;
-		uint64_t n = (uint64_t)input_n;
 		while (p <= n) {
 			result = result * result;
 			if (p * 2 > n) {
@@ -31,24 +31,37 @@ public:
 			}
 			p = p * 2;
 		}
-		
+
 		uint64_t n_remainder = (n > p) ? n - p : 0;
 		if (n_remainder > 0) {
 			result = result * myPow(x, n_remainder);
 		}
 
-		// (-3) ^ 2 = 9
-		// (-3) ^ 3 = -27
+		// (-3) ^ 2 = 9, (-3) ^ 3 = -27
 		if (is_num_negative) {
 			if (n % 2 != 0) {
 				result = -result;
 			}
 		}
-		
+
 		if (is_power_negative) {
 			result = (long double)1 / result;
 		}
-		
+
 		return result;
 	}
 };
+
+int main() {
+	Solution solver;
+	while (!std::cin.eof()) {
+		double x;
+		int n;
+
+		std::cin >> x;
+		std::cin >> n;
+
+		std::cout << solver.myPow(x, n) << std::endl;
+	}
+	return 0;
+}
